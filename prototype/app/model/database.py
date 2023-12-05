79d8d8eb1c255659b33d73d0b9dace37a6cad4f0
nkurzmann <93911059+nkurzmann@users.noreply.github.com>
2023-12-05 15:17:34 +0100

    Create database.py
    The Database Connection

---
+++ prototype/app/model/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+USER=os.getenv("DB_USER")
+PASSWORD=os.getenv("DB_PASSWORD")
+HOST=os.getenv("DB_HOST")
+PORT=os.getenv("DB_PORT")
+NAME=os.getenv("DB_NAME")
+
+
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
