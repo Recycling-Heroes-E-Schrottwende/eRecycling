43a9dd6e3d20fc4281ddda7a9f0ad0667b588ae4
nkurzmann <93911059+nkurzmann@users.noreply.github.com>
2023-12-05 15:18:49 +0100

    Create models.py
    Die Models unserer Datenbank

---
+++ prototype/app/model/models.py
@@ -0,0 +1,47 @@
+from sqlalchemy import Column, Integer, String, TIMESTAMP, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, index=True)
+    password = Column(String)
+    email = Column(String, unique=True, index=True)
+    created_at = Column(TIMESTAMP)
+
+    products = relationship("Product", back_populates="user")
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    product_name = Column(String, index=True)
+    price = Column(Integer)
+    postcode = Column(String)
+    location = Column(String)
+    condition = Column(String)
+    technical_details = Column(String)
+    description = Column(String)
+    details = Column(String)
+    transfer_method = Column(String)
+    created_at = Column(TIMESTAMP, server_default=func.now())
+
+    user = relationship("User", back_populates="products")
+    pictures = relationship("Picture", back_populates="product")
+
+class Picture(Base):
+    __tablename__ = "pictures"
+
+    id = Column(Integer, primary_key=True, index=True)
+    product_id = Column(Integer, ForeignKey("products.id"))
+    picture_name = Column(String, index=True)
+    created_at = Column(TIMESTAMP, server_default=func.now())
+
+    product = relationship("Product", back_populates="pictures")
