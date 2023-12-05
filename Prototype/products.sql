35584398059977b20e799138a5a833c5237fab4e
nkurzmann <93911059+nkurzmann@users.noreply.github.com>
2023-12-05 14:20:50 +0100

    Delete Prototype directory

--- Prototype/products.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Produkte(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    preis VARCHAR(255) NOT NULL,
-    beschreibung VARCHAR(255) NOT NULL,
-    bild bytea NOT NULL
-)
