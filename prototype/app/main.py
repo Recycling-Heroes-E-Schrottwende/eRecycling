295c57ac63ec200601a04f53d13ced45e4461664
nkurzmann <nkurzmann@student.tgm.ac.at>
2024-01-22 14:04:19 +0100

    Implemented PUT Methodes

--- prototype/app/main.py
+++ prototype/app/main.py
@@ -12,6 +12,12 @@ from model import models
 from model import shemas
 from model import posts
 from model import deletes
+from model.shemas import User
+from model.updates import UpdateUser
+from model.updates import UpdateProduct
+from model.shemas import Product
+from model.updates import UpdateImage
+from model.shemas import Image
 
 app = FastAPI()
 
@@ -81,56 +87,29 @@ async def delete_user(user_id: int, db: Session = Depends(get_db)):
 async def delete_product(product_id: int, db: Session = Depends(get_db)):
     deleted_product =  deletes.DeleteProduct(product_id, db)
     return {"message": f"Product {deleted_product.id} deleted successfully"}
-from sqlalchemy import Column, Integer, String, TIMESTAMP, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from sqlalchemy.sql import func
-
-
-Base = declarative_base()
-
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, index=True)
-    password = Column(String)
-    email = Column(String, unique=True, index=True)
-    created_at = Column(TIMESTAMP)
-
-    products = relationship("Product", back_populates="user", cascade="all, delete-orphan")
-
-class Product(Base):
-    __tablename__ = "products"
-
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
-    product_name = Column(String, index=True)
-    price = Column(Integer)
-    postcode = Column(String)
-    location = Column(String)
-    condition = Column(String)
-    technical_details = Column(String)
-    description = Column(String)
-    details = Column(String)
-    transfer_method = Column(String)
-    created_at = Column(TIMESTAMP, server_default=func.now())
-
-    user = relationship("User", back_populates="products")
-    images = relationship("Image", back_populates="product", cascade="all, delete-orphan")
-
-class Image(Base):
-    __tablename__ = "images"
-
-    id = Column(Integer, primary_key=True, index=True)
-    product_id = Column(Integer, ForeignKey("products.id"))
-    image_location = Column(String)
-    created_at = Column(TIMESTAMP, server_default=func.now())
-
-    product = relationship("Product", back_populates="images")
-
 
 @app.delete("/delete_image/{image_id}")
 async def delete_image(image_id: int, db: Session = Depends(get_db)):
     deleted_image =  deletes.DeleteImage(image_id, db)
     return {"message": f"Image {deleted_image.id} deleted successfully"}
+
+@app.put("/update_user/{user_id}")
+async def update_user(user_id: int, user_update: User, db: Session = Depends(get_db)):
+    updated_user = UpdateUser(user_id, user_update, db).update_user()
+    if updated_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return updated_user
+
+@app.put("/update_product/{product_id}")
+async def update_product(product_id: int, product_update: Product, db: Session = Depends(get_db)):
+    updated_product = UpdateProduct(product_id, product_update, db).update_product()
+    if updated_product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return updated_product
+
+@app.put("/update_image/{image_id}")
+async def update_image(image_id: int, image_update: Image, db: Session = Depends(get_db)):
+    updated_image = UpdateImage(image_id, image_update, db).update_image()
+    if updated_image is None:
+        raise HTTPException(status_code=404, detail="Image not found")
+    return updated_image
