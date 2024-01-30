e70955bfd938398f9d56cf9bd8d0f3663028dc0c
jdoppelhofer <jdoppelhofer@student.tgm.ac.at>
2024-01-30 15:01:40 +0100

    Refactor get_product_by_id method in ProductService
    Add UserService class to services.py

--- app/services.py
+++ app/services.py
@@ -10,11 +10,12 @@ class ProductService:
 
     def get_product_by_id(self, product_id):
         response = requests.get(self.url + f"products/?product_id={product_id}")
-        print(response.json())
         if response.status_code == 200:
-            return response.json()
-        else:
-            return {'error': 'Product not found or failed to load'}, 404
+            data = response.json()
+            if isinstance(data, list) and len(data) == 1:
+                return data[0]
+            else:
+                return {'error': 'Product not found or more than one product found'}, 404
 
     def get_image(self, product_id):
         response = requests.get(self.url + f"images/image_location/{product_id}")
@@ -26,3 +27,7 @@ class ProductService:
     def post_product(self, product_data):
         response = requests.post(self.url + "products/", json=product_data)
         return response.json()
+
+class UserService:
+    def __init__(self):
+        self.url = "http://app.recyclingheroes.at/"
