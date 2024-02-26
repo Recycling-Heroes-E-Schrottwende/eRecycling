beaa5ef93f0d0aa93fe4029b984c3b87092927b4
jdoppelhofer <jdoppelhofer@student.tgm.ac.at>
2024-02-26 14:46:52 +0100

    checkout

--- app/app.py
+++ app/app.py
@@ -2,22 +2,23 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from services import ProductService
 
-app = Flask(__name__)
-CORS(app)
+class App:
+    app = Flask(__name__)
+    CORS(app)
 
-product_service = ProductService()
+    product_service = ProductService()
 
-@app.route('/products')
-def products():
-    return jsonify(product_service.get_products())
+    @app.route('/products')
+    def products():
+        return jsonify(product_service.get_products())
 
-@app.route('/product/<int:product_id>')
-def product_detail(product_id):
-    return jsonify(product_service.get_product_by_id(product_id))
+    @app.route('/product/<int:product_id>')
+    def product_detail(product_id):
+        return jsonify(product_service.get_product_by_id(product_id))
 
-@app.route('/image/<int:product_id>')
-def product_image(product_id):
-    return jsonify(product_service.get_image(product_id))
+    @app.route('/image/<int:product_id>')
+    def product_image(product_id):
+        return jsonify(product_service.get_image(product_id))
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=3829)
+    App.app.run(host='localhost', port=3829)
