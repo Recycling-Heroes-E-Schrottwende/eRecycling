f33fbacbaaaaaef46e2b10020e7b41846d0f244d
jdoppelhofer <jdoppelhofer@student.tgm.ac.at>
2024-03-18 13:55:44 +0100

    Aktualisiere Flutter-Abhängigkeiten

--- diplomprojekt/.dart_tool/flutter_build/846120b4c5c0513ff1e25f1352e8304a/web_plugin_registrant.dart
+++ diplomprojekt/.dart_tool/flutter_build/846120b4c5c0513ff1e25f1352e8304a/web_plugin_registrant.dart
@@ -11,6 +11,7 @@ import 'package:file_picker/_internal/file_picker_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:firebase_storage_web/firebase_storage_web.dart';
+import 'package:flutter_image_compress_web/flutter_image_compress_web.dart';
 import 'package:google_sign_in_web/google_sign_in_web.dart';
 import 'package:image_picker_for_web/image_picker_for_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
@@ -26,6 +27,7 @@ void registerPlugins([final Registrar? pluginRegistrar]) {
   FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
   FirebaseStorageWeb.registerWith(registrar);
+  FlutterImageCompressWeb.registerWith(registrar);
   GoogleSignInPlugin.registerWith(registrar);
   ImagePickerPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
